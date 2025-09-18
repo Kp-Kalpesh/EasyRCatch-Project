a3d75e4ddb4a8cc293b0faa59e114c3c58e73de1
Kp-Kalpesh <kalpeshpatil089136@gmail.com>
2025-09-18 14:51:36 +0530

    TesCases Automated of Survey Page

--- src/main/java/easyr/qa/pages/EasyRSurveyPage.java
+++ src/main/java/easyr/qa/pages/EasyRSurveyPage.java
@@ -1,5 +1,9 @@
 package easyr.qa.pages;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -42,11 +46,11 @@ public class EasyRSurveyPage extends TestBase{
 	WebElement chooseFile;
 	@FindBy(xpath = "//button[contains(text(),'Save Image')]")
 	WebElement saveImgBtn;
-	@FindBy(xpath = "//div[contains(@id,'react-select-24-placeholder')]")
+	@FindBy(xpath = "(//div[@class='css-17uqnl1'])[1]")
 	WebElement selectAge;
-	@FindBy(xpath = "//div[contains(@id,'react-select-25-placeholder')]")
+	@FindBy(xpath = "//div[text()='Select Gender']")
 	WebElement selectGender;
-	@FindBy(xpath = "//div[contains(@id,'react-select-26-placeholder')]")
+	@FindBy(xpath = "//div[text()='Select Language']")
 	WebElement selectLang;
 	@FindBy(xpath = "//input[contains(@placeholder,'Enter City or State')]")
 	WebElement enterlocation;
@@ -60,8 +64,33 @@ public class EasyRSurveyPage extends TestBase{
 	WebElement queInArab;
 	@FindBy(xpath = "//span[text()='Save & Continue']")
 	WebElement saveAndContinue;
+	//Choose Among elements
+	@FindBy(xpath = "//input[@placeholder='Enter option 1 in English']")
+	WebElement option1InEng;
+	@FindBy(xpath = "//input[@placeholder='Enter option 2 in English']")
+	WebElement option2InEng;
+	@FindBy(xpath = "//input[@placeholder='Enter option 3 in English']")
+	WebElement option3InEng;
+	@FindBy(xpath = "//input[@placeholder='Enter option 1 in Danish']")
+	WebElement option1InDan;
+	@FindBy(xpath = "//input[@placeholder='Enter option 2 in Danish']")
+	WebElement option2InDan;
+	@FindBy(xpath = "//input[@placeholder='Enter option 3 in Danish']")
+	WebElement option3InDan;
+	@FindBy(xpath = "//input[@placeholder='Enter option 1 in Arabic']")
+	WebElement option1InArab;
+	@FindBy(xpath = "//input[@placeholder='Enter option 2 in Arabic']")
+	WebElement option2InArab;
+	@FindBy(xpath = "//input[@placeholder='Enter option 3 in Arabic']")
+	WebElement option3InArab;
 	@FindBy(xpath = "//button[text()='Create Survey']")
 	WebElement CreateSurveyButton;
+	@FindBy(xpath = "//div[@id='swal2-html-container']")
+	WebElement messageBeforeCreateSurvey;
+	@FindBy(xpath = "//button[text()='Create it']")
+	WebElement clickOnCreateItBtn;
+	@FindBy(xpath = "//button[text()='No, cancel!']")
+	WebElement clickOnNoCancelBtn;
 	@FindBy(xpath = "//input[@placeholder='Search Survey by Title, Vendor...']")
 	WebElement searchField;
 	@FindBy(xpath = "//h6[contains(@class,'mb-0 capitalize font-semibold lea')]")
@@ -91,91 +120,199 @@ public class EasyRSurveyPage extends TestBase{
 		return createNewSurveyTxt.getText();
 	}
 
-	public void enterTitleInEng() {
-		act.sendKeys(titleInEng, "Test").perform();
+	public void enterTitleInEng(String surveyTitleinEng) {
+		act.sendKeys(titleInEng, surveyTitleinEng).perform();
 	}
 
-	public void enterTitleInDan() {
-		act.sendKeys(titleInDan, "test").perform();
+	public void enterTitleInDan(String surveyTitleinDan) {
+		act.sendKeys(titleInDan, surveyTitleinDan).perform();
 	}
 
-	public void enterTitleInArab() {
-		act.sendKeys(titleInArab, "test").perform();
+	public void enterTitleInArab(String surveyTitleinArab) {
+		act.sendKeys(titleInArab, surveyTitleinArab).perform();
 	}
 
-	public void selectVendor() throws InterruptedException {
-		act.sendKeys(selectVendor, "Joss Sterlin").perform();
+	public void selectVendor(String vName) throws InterruptedException {
+		act.sendKeys(selectVendor, vName).perform();
 		Thread.sleep(1000);
 		act.sendKeys(Keys.ENTER).perform();
 	}
 
-	public void enterRewardKroners() {
-		act.sendKeys(rewardKroner, "10").perform();
+	public void enterRewardKroners(String rewardKrones) {
+		act.sendKeys(rewardKroner, rewardKrones).perform();
 	}
 
-	public void enterUserLimit() {
-		act.sendKeys(userLimit, "5").perform();
+	public void enterUserLimit(String usersLimit) {
+		act.sendKeys(userLimit, usersLimit).perform();
 	}
 
-	//	public void selectStartDate() {
-	//		startDate.click();
-	//	}
-	//	
-	//	public void selectEndDate() {
-	//		endDate.click();
-	//	}
+	public void selectStartDate() throws InterruptedException {
+		Thread.sleep(1000);
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d"); // only day number
+		String currentDay = today.format(formatter);
+
+		// Click on date picker input
+		startDate.click();
+		WebElement todayDate = driver.findElement(By.xpath("(//span[@class='flatpickr-day today'] [text()='" + currentDay + "'])[1]"));
+		todayDate.click();													
+		boolean isPastDateDisabled = driver.findElements(By.xpath("(//span[contains(@class,'flatpickr-day') and text()='" + (today.getDayOfMonth() - 1) + "'])[1]")).size() > 0;
+
+		if (isPastDateDisabled) {
+			System.out.println("Past dates are disabled, only current date is selectable.");
+		} else {
+			System.out.println("Past dates are still selectable!");
+		}
+	}
+
+	public void selectEndDate() {
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d"); // only day number
+		String currentDay = today.format(formatter);
+
+		// Click on Expiry date picker input
+		endDate.click();
+		WebElement todayDate = driver.findElement(By.xpath("(//span[text()='" + currentDay + "'])[2]"));															
+		todayDate.click();
+	}
 
 	public void clickOnUploadImgBtn() {
 		uplaodImgBtn.click();
 	}
 
-	public void chooseimgFile() {
-		chooseFile.sendKeys("path of img file");
+	public void chooseimgFile(String surveyImg) {
+		chooseFile.sendKeys(surveyImg);
 	}
 
 	public void clickOnSaveimgBtn() {
 		saveImgBtn.click();
 	}
 
-	public void selectAge() throws InterruptedException {
-		act.sendKeys(selectAge, "26-35 Years").perform();
+	public void selectAge(String age) throws InterruptedException {
+		act.click(selectAge).perform();
+		act.sendKeys(selectAge, age).perform();
+		Thread.sleep(1000);
+		act.sendKeys(Keys.ENTER).perform();
+		act.sendKeys(Keys.TAB).perform();
+	}
+
+	public void selectGender(String gender) throws InterruptedException {
+		act.sendKeys(selectGender, gender).perform();
+		Thread.sleep(1000);
+		act.sendKeys(Keys.ENTER).perform();
+		act.sendKeys(Keys.TAB).perform();
+	}
+
+	public void selectLanguage(String lang) throws InterruptedException {
+		act.sendKeys(selectLang, lang).perform();
 		Thread.sleep(1000);
 		act.sendKeys(Keys.ENTER).perform();
 	}
 
-	public void selectGender() throws InterruptedException {
-		act.sendKeys(selectGender, "Male").perform();
+	public void selectLocation(String location) throws InterruptedException {
+		act.click(enterlocation).perform();
 		Thread.sleep(1000);
+		act.sendKeys(enterlocation, location).perform();
+		Thread.sleep(3000);
+		act.sendKeys(Keys.ARROW_DOWN).perform();
 		act.sendKeys(Keys.ENTER).perform();
 	}
 
-	public void selectLanguage() throws InterruptedException {
-		act.sendKeys(selectLang, "English").perform();
-		Thread.sleep(1000);
+	//select question type 1
+	public void selectQuestionTypeTrueorFalse(String queType_1) {
+		act.sendKeys(selectQuestionType, queType_1).perform();
 		act.sendKeys(Keys.ENTER).perform();
 	}
 
-	public void selectLocation() {
-		act.sendKeys(enterlocation, "sakri").perform();
+	public void enterQuestionInEng(String que_1InEng) {
+		act.sendKeys(queInEng, que_1InEng).perform();
 	}
 
-	public void selectQuestionType() {
-		act.sendKeys(selectQuestionType, "true or false").perform();
+	public void enterQuestionInDan(String que_1InDan) {
+		act.sendKeys(queInDan, que_1InDan).perform();
 	}
 
-	public void enterQuestionInEng() {
-		act.sendKeys(queInEng, "Test").perform();
+	public void enterQuestionInArab(String que_1InArab) {
+		act.sendKeys(queInArab, que_1InArab).perform();
 	}
 
-	public void enterQuestionInDan() {
-		act.sendKeys(queInDan, "Test").perform();
+	public void clickOnSaveAndContinueForQue1() {
+		saveAndContinue.click();
 	}
 
-	public void enterQuestionInArab() {
-		act.sendKeys(queInArab, "Test").perform();
+	//select question type 2
+	public void selectQuestionTypeRating(String queType_2) {
+		act.sendKeys(selectQuestionType, queType_2).perform();
+		act.sendKeys(Keys.ENTER).perform();
 	}
 
-	public void clickOnSaveAndContinue() {
+	public void selectQuestionTypeRatingInEng(String que_2InEng) {
+		act.sendKeys(queInEng, que_2InEng).perform();
+	}
+
+	public void selectQuestionTypeRatingInDan(String que_2InDan) {
+		act.sendKeys(queInDan, que_2InDan).perform();
+	}
+
+	public void selectQuestionTypeRatingInArab(String que_2InArab) {
+		act.sendKeys(queInArab, que_2InArab).perform();
+	}
+
+	public void clickOnSaveAndContinueForQue2() {
+		saveAndContinue.click();
+	}
+
+	//select question type 3
+	public void selectQuestionTypeComment(String queType_3) {
+		act.sendKeys(selectQuestionType, queType_3).perform();
+		act.sendKeys(Keys.ENTER).perform();
+	}
+
+	public void selectQuestionTypeCommentInEng(String que_3InEng) {
+		act.sendKeys(queInEng, que_3InEng).perform();
+	}
+
+	public void selectQuestionTypeCommentInDan(String que_3InDan) {
+		act.sendKeys(queInDan, que_3InDan).perform();
+	}
+
+	public void selectQuestionTypeCommentInArab(String que_3InArab) {
+		act.sendKeys(queInArab, que_3InArab).perform();
+	}
+
+	public void clickOnSaveAndContinueForQue3() {
+		saveAndContinue.click();
+	}
+
+
+	//select question type 4
+	public void selectQuestionTypeChooseAmong(String queType_4) {
+		act.sendKeys(selectQuestionType, queType_4).perform();
+		act.sendKeys(Keys.ENTER).perform();
+	}
+
+	public void selectQuestionTypeChooseAmongInEng(String que_4InEng, String opt_1InEng, String opt_2InEng, String opt_3InEng) {
+		act.sendKeys(queInEng, que_4InEng).perform();
+		option1InEng.sendKeys(opt_1InEng);
+		option2InEng.sendKeys(opt_2InEng);
+		option3InEng.sendKeys(opt_3InEng);
+	}
+
+	public void selectQuestionTypeChooseAmongInDan(String que_4InDan, String opt_1InDan, String opt_2InDan, String opt_3InDan) {
+		act.sendKeys(queInDan, que_4InDan).perform();
+		option1InDan.sendKeys(opt_1InDan);
+		option2InDan.sendKeys(opt_2InDan);
+		option3InDan.sendKeys(opt_3InDan);
+	}
+
+	public void selectQuestionTypeChooseAmongInArab(String que_4InArab, String opt_1InArab, String opt_2InArab, String opt_3InArab) {
+		act.sendKeys(queInArab, que_4InArab).perform();
+		option1InArab.sendKeys(opt_1InArab);
+		option2InArab.sendKeys(opt_2InArab);
+		option3InArab.sendKeys(opt_3InArab);
+	}
+
+	public void clickOnSaveAndContinueForQue4() {
 		saveAndContinue.click();
 	}
 
@@ -183,8 +320,17 @@ public class EasyRSurveyPage extends TestBase{
 		CreateSurveyButton.click();
 	}
 
-	public void sendDataInSearchField() {
-		act.sendKeys(searchField, "created survey text").perform();
+	public String verifyMessageOnModalBeforeCreatingSurvey() {	
+		return messageBeforeCreateSurvey.getText();
+	}
+
+	public void clickOnCreateItBtn() throws InterruptedException {
+		clickOnCreateItBtn.click();
+		Thread.sleep(3000);
+	}
+
+	public void sendDataInSearchField(String SurveyTitle) {
+		act.sendKeys(searchField, SurveyTitle).perform();
 	}
 
 	public String verifyCreatedSurveyTitle() {
@@ -194,6 +340,4 @@ public class EasyRSurveyPage extends TestBase{
 	public String verifySelectedVendor() {
 		return vendorName.getText();
 	}
-
-
 }
--- src/test/java/easyr/qa/testCases/EasyRSurveyPageTest.java
+++ src/test/java/easyr/qa/testCases/EasyRSurveyPageTest.java
@@ -3,6 +3,7 @@ package easyr.qa.testCases;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import easyr.qa.base.TestBase;
 import easyr.qa.pages.EasyRCatchShopPage;
@@ -13,6 +14,7 @@ import easyr.qa.pages.EasyROffersAndDealsPage;
 import easyr.qa.pages.EasyROrganizationPage;
 import easyr.qa.pages.EasyRSurveyPage;
 import easyr.qa.pages.EasyRVendorPage;
+import junit.framework.Assert;
 
 public class EasyRSurveyPageTest extends TestBase{
 
@@ -46,6 +48,69 @@ public class EasyRSurveyPageTest extends TestBase{
 	@Test(priority = 1)
 	public void verifySurvey() {
 		try {
+			Assert.assertEquals(survpg.verifySurveyTxtOnPage(), "SURVEY");
+			survpg.clickOnCreateSurveyBtn();
+			survpg.verifyCreateSurveyTxt();
+			survpg.enterTitleInEng(prop.getProperty("SurveyTitle_English"));
+			survpg.enterTitleInDan(prop.getProperty("SurveyTitle_Danish"));
+			survpg.enterTitleInArab(prop.getProperty("SurveyTitle_Arabic"));
+			survpg.selectVendor(prop.getProperty("SurveyVendorName"));
+			survpg.enterRewardKroners(prop.getProperty("RewardsInKrones"));
+			survpg.enterUserLimit(prop.getProperty("UserLimit"));
+			survpg.selectStartDate();
+			survpg.selectEndDate();
+			survpg.clickOnUploadImgBtn();
+			survpg.chooseimgFile(prop.getProperty("SurveyImgFile"));
+			survpg.clickOnSaveimgBtn();
+			//						survpg.selectAge(prop.getProperty("UsersAge"));
+			//						survpg.selectGender(prop.getProperty("UsersGender"));
+			//						survpg.selectLanguage(prop.getProperty("UserLanguage"));
+			survpg.selectLocation(prop.getProperty("userLocation"));
+			//Question 1
+			survpg.selectQuestionTypeTrueorFalse(prop.getProperty("QuestionType_1"));
+			survpg.enterQuestionInEng(prop.getProperty("Question_1InEnglish"));
+			survpg.enterQuestionInDan(prop.getProperty("Question_1InDansh"));
+			survpg.enterQuestionInArab(prop.getProperty("Question_1InArabic"));
+			survpg.clickOnSaveAndContinueForQue1();
+			//Question 2
+			survpg.selectQuestionTypeRating(prop.getProperty("QuestionType_2"));
+			survpg.selectQuestionTypeRatingInEng(prop.getProperty("Question_2InEnglish"));
+			survpg.selectQuestionTypeRatingInDan(prop.getProperty("Question_2InDansh"));
+			survpg.selectQuestionTypeRatingInArab(prop.getProperty("Question_2InArabic"));
+			survpg.clickOnSaveAndContinueForQue2();
+			//Question 3
+			survpg.selectQuestionTypeComment(prop.getProperty("QuestionType_3"));
+			survpg.selectQuestionTypeCommentInEng(prop.getProperty("Question_3InEnglish"));
+			survpg.selectQuestionTypeCommentInDan(prop.getProperty("Question_3InDansh"));
+			survpg.selectQuestionTypeCommentInArab(prop.getProperty("Question_3InArabic"));
+			survpg.clickOnSaveAndContinueForQue3();
+			//Question 3
+			survpg.selectQuestionTypeChooseAmong(prop.getProperty("QuestionType_4"));
+			survpg.selectQuestionTypeChooseAmongInEng(prop.getProperty("Question_4InEnglish"), prop.getProperty("option_1InEng"), prop.getProperty("option_2InEng"), prop.getProperty("option_3InEng"));
+			survpg.selectQuestionTypeChooseAmongInDan(prop.getProperty("Question_4InDansh"), prop.getProperty("option_1InDan"), prop.getProperty("option_2InDan"), prop.getProperty("option_3InDan"));
+			survpg.selectQuestionTypeChooseAmongInArab(prop.getProperty("Question_4InArabic"), prop.getProperty("option_1InArab"), prop.getProperty("option_2InArab"), prop.getProperty("option_3InArab"));
+			survpg.clickOnSaveAndContinueForQue4();
+			survpg.clickOnCreatSurveyButton();
+			System.out.println("Message on modal: " + survpg.verifyMessageOnModalBeforeCreatingSurvey());
+			Assert.assertEquals(survpg.verifyMessageOnModalBeforeCreatingSurvey(), "Today's date selected. Start date cannot be modified after survey creation.");
+			survpg.clickOnCreateItBtn();
+
+		}catch (Exception e) {
+			System.out.println("Element not found within timeout: " + e.getMessage());
+		}
+	}
+
+
+	@Test(priority = 2)
+	public void verifyCreatedSurveyOnListingPage() {
+		try {
+			SoftAssert sa = new SoftAssert();
+			survpg.sendDataInSearchField(prop.getProperty("SurveyTitle_English"));
+			System.out.println("verifyCreatedSurveyTitle(): " + survpg.verifyCreatedSurveyTitle());
+			sa.assertEquals(survpg.verifyCreatedSurveyTitle(), "Car Satisfaction Survey\r\n"
+					+ "4");
+			System.out.println("survpg.verifySelectedVendor(): " + survpg.verifySelectedVendor());
+			Assert.assertEquals(survpg.verifySelectedVendor(), "Joss Sterlin (Sterlin Industries)");
 
 		}catch (Exception e) {
 			System.out.println("Element not found within timeout: " + e.getMessage());
@@ -54,6 +119,6 @@ public class EasyRSurveyPageTest extends TestBase{
 
 	@AfterMethod
 	public void tearDown() {
-		//		driver.quit();s
+		//		driver.quit();
 	}
 }
